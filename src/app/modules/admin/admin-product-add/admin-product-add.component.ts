80513059772fe33ee71b8c5d4cb0e18ed69d6118
jacek <jacek@szulc.cc>
2023-01-15 12:37:04 +0100

    Input data validation

--- src/app/modules/admin/admin-product-add/admin-product-add.component.ts
+++ src/app/modules/admin/admin-product-add/admin-product-add.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { AdminMessageService } from '../admin-message.service';
 import { AdminProductAddService } from './admin-product-add.service';
 
 @Component({
@@ -16,25 +17,29 @@ export class AdminProductAddComponent implements OnInit {
     private formBuilder: FormBuilder,
     private adminProductAddService: AdminProductAddService,
     private router: Router,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private adminMessageService: AdminMessageService
     ) {}
 
   ngOnInit(): void {
     this.productForm = this.formBuilder.group({
-      name: [''],
-      description: [''],
-      category: [''],
-      price: [''],
-      currency: ['PLN']
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      description: ['', [Validators.required, Validators.minLength(4)]],
+      category: ['', [Validators.required, Validators.minLength(4)]],
+      price: ['', [Validators.required, Validators.min(0)]],
+      currency: ['PLN', [Validators.required]]
     });
   }
 
   submit(){
     this.adminProductAddService.saveNewProduct(this.productForm.value)
-      .subscribe(product => {
+      .subscribe({
+        next: product => {
         this.router.navigate(["/admin/products/update", product.id])
           .then(() => this.snackBar.open("Produkt został dodany", "", {duration: 3000}))
-      })
+      },
+      error: err => this.adminMessageService.addSpringErrors(err.error)
+    })
   }
 
 }

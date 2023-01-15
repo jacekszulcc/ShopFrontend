26340b468f70e1d52a78ef76e27c63f381c743c9
jacek <jacek@szulc.cc>
2023-01-15 14:55:53 +0100

    Product removal

--- src/app/modules/admin/admin-product/admin-product.component.ts
+++ src/app/modules/admin/admin-product/admin-product.component.ts
@@ -1,6 +1,8 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatTable } from '@angular/material/table';
 import { map, startWith, switchMap } from 'rxjs';
+import { AdminConfirmDialogService } from '../admin-confirm-dialog.service';
 import { AdminProductService } from './admin-product.service';
 import { AdminProduct } from './adminProduct';
 
@@ -10,14 +12,20 @@ import { AdminProduct } from './adminProduct';
   styleUrls: ['./admin-product.component.scss']
 })
 export class AdminProductComponent implements AfterViewInit{
+
   
   //dataSource: AdminProduct[] = [];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatTable) table!: MatTable<any>;
   displayedColumns: string[] = ["id", "name", "price", "actions"]
   totalElements: number = 0;
   data: AdminProduct[] = [];
 
-  constructor(private adminProductService: AdminProductService) { }
+
+  constructor(
+    private adminProductService: AdminProductService,
+    private dialogService: AdminConfirmDialogService  
+  ) { }
 
   ngAfterViewInit(): void {
     this.paginator.page.pipe(
@@ -28,6 +36,24 @@ export class AdminProductComponent implements AfterViewInit{
     ).subscribe(data => {
       this.totalElements = data.totalElements;
       this.data = data.content;
+    });
+  }
+
+  confirmDelete(element: AdminProduct) {
+    this.dialogService.openConfirmDialog("Czy na pewno chcesz usunąć produkt?")
+    .afterClosed()
+    .subscribe(result => {
+      if(result) {
+        this.adminProductService.delete(element.id)
+          .subscribe(() => {
+            this.data.forEach((value, index) => {
+              if(element == value) {
+                this.data.splice(index, 1);
+                this.table.renderRows();
+              }
+            })
+          });
+      }
     });
   }
 }
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -8,6 +8,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {MatDialogModule} from '@angular/material/dialog';
 @NgModule({
   declarations: [],
   imports: [
@@ -19,7 +20,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatPaginatorModule,
     MatTableModule,
     MatInputModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatDialogModule
   ],
   exports: [
     MatIconModule,
@@ -29,7 +31,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatPaginatorModule,
     MatTableModule,
     MatInputModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatDialogModule
   ]
 })
 export class MaterialModule { }

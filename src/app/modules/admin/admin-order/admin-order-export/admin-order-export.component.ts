a3aad22b1f92393d60a797bf42c4fd23cdc59855
Jacek Szulc <jacek@szulc.cc>
2023-03-28 20:50:07 +0200

    Export of orders

---
+++ src/app/modules/admin/admin-order/admin-order-export/admin-order-export.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-order-export',
+  templateUrl: './admin-order-export.component.html',
+  styleUrls: ['./admin-order-export.component.scss']
+})
+export class AdminOrderExportComponent {
+
+}
